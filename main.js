a3154e6dc82defc0164d245e053964f04f1f4571
swakewatson <swakewatson@users.noreply.github.com>
2017-08-09 12:26:54 +0100

    Cleaning and Commenting
    Removing useless code and adding more thorough comments.

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 //Import necessary modules and variables
+
+//.env contains files that the botframework uses to validate the bot
+//These are defined on the botframework website
 require('dotenv-extended').load();
+//DatabaseTools is a file to manage the JSON database the bot uses to store users and reports
 var db = require('./databaseTools');
+//Botbuilder and restify are node.js modules necessary for the bot and use of skype features
 var builder = require('botbuilder');
 var restify = require('restify');
+//This is used to schedule when to run statusUpdates and get reports
 var schedule = require('node-schedule');
+//This is used to date reports
 var datetime = require('node-datetime');
 
-var commands = ["!teamStatus", "!help", "!updateStatus", "!teamReset", "!assignTeams", "!forceStatusUpdate", "!submitReport", "!forceSubmitReport", "!viewReports", "!userReport"]
 
+//List of commands - used to check if the user has inputted a command
+//New commands must be added to this list or they will not run
+var commands = ["!teamStatus", "!help", "!updateStatus", "!teamReset", "!assignTeams", "!forceStatusUpdate", "!submitReport", "!forceSubmitReport", "!userReport"]
+
+
+//Setting up the skype bot
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -24,36 +36,39 @@ server.post('/api/messages', connector.listen());
 //set up bot
 var bot = new builder.UniversalBot(connector);
 
-//building cards
+
+
+
+
+//Building cards and carousels - this is the primary way of displaying information
+//Card used in !teamStatus function - allows report to be displayed to user upon pressing button
 function userCard(session, connector, name, workingStatus, skypeID) {
 	var card = new builder.HeroCard(session)
 		.title(name)
 		.subtitle(workingStatus.toString())
 		.buttons([
-		//edit this
 			builder.CardAction.dialogAction(session, 'userReport', skypeID, 'See Report')
         ]);
 	return card;
 }
 
+//Card used to add users without a team to a predefined team
 function userTeamAssignCard(session, connector, name, workingStatus, skypeID) {
 	var card = new builder.HeroCard(session)
 		.title(name)
 		.subtitle(workingStatus.toString())
 		.buttons([
+			//This button should be replaced. dialogAction should be used instead
+			//See the userCard card above for an example
             builder.CardAction.postBack(session, skypeID, 'Add ' + name + " to team")
         ]);
 	return card;
 }
 
-function reportCard(session, connector, name, yesterdayText, todayText) {
-	var card = new builder.HeroCard(session)
-		.title(name)
-		.subtitle(yesterdayText)
-		.text(todayText);
-	return card;
-}	
 
+//Producing carousels from a list of objects 
+//Due to limits w/ carousels, only two cards are displayed on each carousel and multiple carousels are outputted
+//The code that produces the multiple carousels is currently contained in dialogs
 function teamStatus(session, connector, teamArray) {
 	var msg = new builder.Message(session)
 		.attachmentLayout(builder.AttachmentLayout.carousel);
@@ -72,15 +87,12 @@ function teamAssign(session, connector, teamArray) {
 	return msg;
 }
 
-function reportCarousel(session, connector, reportArray) {
-	var msg = new builder.Message(session)
-		.attachmentLayout(builder.AttachmentLayout.carousel);
-	for (a = 0; a < reportArray.length; a++) {
-		msg.addAttachment(reportCard(session, connector, reportArray[a].name, reportArray[a].yesterdayText, reportArray[a].todayText));
-	}
-	return msg;
-}
 
+
+
+//Creating 'classes' to handle the user and report objects
+//SkypeID is used as the main identifier for the users - reports have no unique ID. 
+//If multiple reports are submitted on a day, only one will be accessable to user
 // Create employee 'class' 
 function employee(skypeID, address, name, team) {
 	this.skypeID = skypeID;
@@ -102,6 +114,10 @@ function report(skypeID, team, yesterdayText, todayText, issueText) {
 	this.issueText = issueText;
 }
 
+
+//Bot dialogs - all commands use a dialog
+
+//Add a user upon their first time messaging the bot - run if root dialog doesn't recognise their skypeID
 bot.dialog('/addFirstTimeUser', [
 	function (session) {
 		var newUserName;
@@ -126,35 +142,7 @@ bot.dialog('/introduce', function (session) {
 	session.endDialog();
 });
 
-//currently defunct - the other function replaces it
-bot.dialog('/updateStatus', [
-	function (session) {
-		var skypeID;
-		var newStatus;
-		builder.Prompts.text(session, "Input SkypeID of user")
-	},
-	function (session, results) {
-		skypeID = results.response;
-		var msg = new builder.Message(session) //use beginDialogue() for this in working program OR replace it with a builder.Prompts.choice with the necessary options
-			.text("Good Morning User")
-			.suggestedActions(
-				builder.SuggestedActions.create(
-						session, [
-							builder.CardAction.postBack(session, "In", "In"),
-							builder.CardAction.postBack(session, "Out", "Home"),
-							builder.CardAction.postBack(session, "Holiday", "Holiday")
-						]
-					));
-		builder.Prompts.text(session, msg);
-	},
-	function (session, results) {
-		newStatus = results.response;
-		db.updateStatus(skypeID, newStatus);
-		session.endDialog("Status updated");
-	}
-]);
-
-//wipes the team values
+//Wipes the team values - if new teams need to be assigned this can be run, followed by !assignTeams
 bot.dialog('/teamReset', [
 	function (session) {
 		var response;
@@ -173,12 +161,13 @@ bot.dialog('/teamReset', [
 	}
 ]);
 
-//requests update on the status of the user it sends this dialog to
+//Requests update on the status of the user it sends this dialog to
 bot.dialog('/updateUserStatus', [
 	function (session) { 
 		var msg = new builder.Message(session)
 			.text("Good Morning User. Please input your working status today (In, Working from home or on Holiday)")
 			.suggestedActions(
+				//Suggested actions does not actually work yet - this is displayed as a normal message
 				builder.SuggestedActions.create(
 						session, [
 							builder.CardAction.postBack(session, "In", "In"),
@@ -196,6 +185,8 @@ bot.dialog('/updateUserStatus', [
 	}
 ]);
 
+//Used for assigning teams
+//Needs to be improved teamAssignCards should use dialogActions, so multiple users can be added at once
 bot.dialog('/assignTeams', [
 	function (session) {
 		var team;
@@ -215,6 +206,8 @@ bot.dialog('/assignTeams', [
 	}
 ]); 
 
+//Resets all statuses and requests statusUpdates from all users
+//Allows users to effectively run scheduler again on command
 bot.dialog('/forceStatusUpdate', function (session) {
 	db.wipeStatuses();
 	var users = db.findAll("workingStatus", "unknown");
@@ -224,6 +217,7 @@ bot.dialog('/forceStatusUpdate', function (session) {
 	}
 });
 
+//Takes inputs from user and saves them as a new report
 bot.dialog('/submitReport', [
 	function (session) {
 		var yesterdayInput;
@@ -249,6 +243,7 @@ bot.dialog('/submitReport', [
 	}
 ]);
 
+//Requests all users submit another report - similar to !forceStatusUpdate
 bot.dialog('/forceSubmitReport', function (session) {
 	var team = db.findValue(session.message.address.user.id, "team");
 	var users = db.findAll("team", team);
@@ -262,6 +257,7 @@ bot.dialog('/forceSubmitReport', function (session) {
 bot.dialog('/teamStatus', function (session) {
 	var searchTeam = db.findValue(session.message.address.user.id, "team");
 	var teamArray = db.findAll("team", searchTeam);
+	//This section handles the outputting of multiple carousels to avoid skype's constraints
 	var number = teamArray.length / 2;
 	var carouselsNeeded = Math.ceil(number);
 	for (i = 0; i < carouselsNeeded; i++) {
@@ -277,58 +273,32 @@ bot.dialog('/teamStatus', function (session) {
 	session.endDialog();
 });
 
-bot.dialog('/viewReports', function (session) {
-	var team = db.findValue(session.message.address.user.id, "team");
-	var date = datetime.create();
-	date = date.format('d/m/Y');
-	var reports = db.findTeamReports(team, date);
-	var num = reports.length / 2;
-	var carouselsNeeded = Math.ceil(num);
-	for (i = 0; i < carouselsNeeded; i++) {
-		var reportSample = new Array;
-		for (j = 0; j < 2; j++) {
-			var num = (i * 2) + j;
-			if (num >= reports.length) { break; }
-			reportSample.push(reports[num]);
-		}
-		var carousel = reportCarousel(session, connector, reportSample);
-		session.send(carousel);
-	}
-	session.endDialog();
-});
-
+//Outputs the report the user submitted that day
 bot.dialog('/userReport', [ 
 	function (session, args) {
 		var date = datetime.create();
 		date = date.format('d/m/Y');
 		var report = db.findRecentUserReport(args.data, date);
-		//What have you coded here??? delete this
-		console.log(report);
-		for (i = 0; i < 3; i++) {
-			switch(i) {
-				case 0:
-					session.send("Yesterday: " + report.yesterdayText);
-					break;
-				case 1:
-					session.send("Today: " + report.todayText);
-					break;
-				case 2:
-				session.send("Issues: " + report.issueText);
-			}
-		}
+		session.send("Yesterday: " + report.yesterdayText);
+		session.send("Today: " + report.todayText);
+		session.send("Issues: " + report.issueText);
 		session.endDialog();
 	}
 ]);
+//Begins the userReport dialog if the button on the userCard is pressed
 bot.beginDialogAction('userReport', '/userReport');
 
-//SCHEDULER
+
+
+
+
+//Schedulers to get daily status updates and reports from everyone in the DB
 var statusUpdateRule = new schedule.RecurrenceRule();
-//REMEMBER - THESE NEEED TO BE CHANGED AND THEY ALSO NEED TO BE CUSTOMISABLE
-//THATS RIGHT - MORE JSON USAGE, MY FAVOURITE!!!
 statusUpdateRule.minute = 30;
 statusUpdateRule.hour = 9;
 
 var statusUpdateJob = schedule.scheduleJob(statusUpdateRule, function(session) {
+	//Resets all workingStatuses to "unknown", then runs a search to return everyone
 	db.wipeStatuses();
 	var users = db.findAll("workingStatus", "unknown");
 	for (i = 0; i < users.length; i++) {
@@ -337,11 +307,13 @@ var statusUpdateJob = schedule.scheduleJob(statusUpdateRule, function(session) {
 	}
 });
 
+
 var reportUpdateRule = new schedule.RecurrenceRule();
-reportUpdateRule.minute = 50;
-reportUpdateRule.hour = 9;
+reportUpdateRule.minute = 57;
+reportUpdateRule.hour = 15;
 
 var reportUpdateJob = schedule.scheduleJob(reportUpdateRule, function(session) {
+	//All users are using the skype channel, so this returns all users
 	var users = db.findAll("address/channelId", "skype");
 	for (i = 0; i < users.length; i++) {
 		var address = users[i].address;
@@ -350,21 +322,25 @@ var reportUpdateJob = schedule.scheduleJob(reportUpdateRule, function(session) {
 });
 
 
-//This is the root dialog
+
+
+
+//This is the root dialog - the default dialog the skype bot runs, and will return to once other dialogs have finished
 bot.dialog('/', function (session) {
+	//Check if the user is already stored in the DB, if not, run "addFirstTimeUser" and create a new entry for them
 	var savedAddress;
 	savedAddress = session.message.address;
 	var notFirstTime = db.findUser(session.message.address.user.id);
 	if (notFirstTime != true) {
 		bot.beginDialog(savedAddress, '/addFirstTimeUser');
+	//If they already exist, check if their input is a command. Run the introduction if it's not
 	} else if (!commands.includes(session.message.text)) {
-		//POTENTIAL FOR BUGS HERE - TEST
 		savedAddress = db.findValue(session.message.address.user.id, "address");
 		bot.beginDialog(savedAddress, '/introduce');
 	} 
+	//If the user inputs something present in the commands array, determine what they inputted
+	//then run the appropriate dialog
 	else switch(session.message.text) {
-		//THIS IS NOT WORKING DUE TO ADDING AN ADDRESS FIELD TO THE DB - MAY REQUIRE EXTENSIVE WORK - MIGHT NEED TO BE DITCHED COMPLETELY
-		//AS THE USER IS UNABLE TO INPUT ALL OF THE NECESSARY INFORMATION
 		case "!teamStatus":
 			bot.beginDialog(savedAddress, "/teamStatus");
 			break;
@@ -389,12 +365,6 @@ bot.dialog('/', function (session) {
 		case "!forceSubmitReport":
 			bot.beginDialog(savedAddress, "/forceSubmitReport");
 			break;
-		case "!viewReports":
-			bot.beginDialog(savedAddress, "/viewReports");
-			break;
-		/* case "!userReport":
-			bot.beginDialog(savedAddress, "/userReport");
-			break; */	
 	}
 });
 
